142435f7a59458478c38faf8fa39401068b7cc65
jannes <jannes.dev@posteo.net>
2024-10-16 07:40:19 +0200

    chore(wasm): use standard allocator
    Remove unmaintained allocator dependency, make the code
    as minimal as possible.

--- epubparse-wasm/src/lib.rs
+++ epubparse-wasm/src/lib.rs
@@ -1,23 +1,12 @@
-mod utils;
-
-extern crate epubparse;
-
 use epubparse::epub_to_book;
-use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-/// returns either Book converted to JsValue
-/// or ParseError converted to JsValue
+/// returns either
+/// - Book converted to JsValue
+/// - ParseError converted to JsValue
 #[wasm_bindgen]
 pub fn parse_epub(bytes: &[u8]) -> Result<JsValue, JsValue> {
-    set_panic_hook();
     let book = epub_to_book(bytes);
-    book.map(|b| JsValue::from_serde(&b).unwrap())
+    book.map(|b| serde_wasm_bindgen::to_value(&b).unwrap())
         .map_err(|parse_error| JsValue::from_str(&parse_error.to_string()))
 }
